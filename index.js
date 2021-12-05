87fd43dcd992a37348e6d9ced18ed6325651237e
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-12-05 11:53:52 -0300

    Instalando consign e separando responsabilidades

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express');
+const customExpress = require('./config/customExpress');
+
 const port = 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bem vindo!');
-})
+const app = customExpress();
 
 app.listen(port, () => `Application online in port: [${port}]`);
