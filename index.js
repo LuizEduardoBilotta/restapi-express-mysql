7f65344d6dae6f3045906114514573fc7df8729c
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-12-18 21:23:35 -0300

    Refatorando e inserindo console.log no listen

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const Tables = require('./config/tables');
 
 const port = 3000;
 
+
 connectionDB.connect((error) => {
   if(!error) {
     const app = customExpress();
-    new Tables(connectionDB);
-    app.listen(port, () => `[LOG]: Application online in port: [${port}]`);
+    Tables.createSchedule(connectionDB);
+    app.listen(port, () => console.log(`[LOG]: Application online in port: [${port}]`));
   }
 });
+
+
 
 
 
