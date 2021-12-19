43ff76f2cfc5c736917a87f68b8640172f036cd9
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-12-18 21:24:04 -0300

    Criando novas rotas

--- api/controllers/schedules.js
+++ api/controllers/schedules.js
@@ -2,11 +2,27 @@ const schedules = require('../models/schedulesModel');
 
 module.exports = (app) => {
   app.get('/schedules', (req, res) => {
-    res.send('Bem vindo!');
+    schedules.listSchedules(res);
+  });
+
+  app.get('/schedules/:id', (req, res) => {
+    const id = parseInt(req.params.id); 
+    schedules.listSchedulesById(id, res);
+  });
+
+  app.patch('/schedules/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const informations = req.body;
+    schedules.updateScheduleById(id, informations, res);
   });
 
   app.post('/schedules', (req, res) => {
     const schedule = req.body;
     schedules.createSchedule(schedule, res);
   })
+
+  app.delete('/schedules/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    schedules.deleteScheduleById(id, res);
+  });
 }
